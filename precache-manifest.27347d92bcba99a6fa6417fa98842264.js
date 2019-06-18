aa1fe9e7761e33bc8c661415255b3505bd42a25f
Susan Su <susansu.software@gmail.com>
2019-06-18 13:36:37 -0700

    Updates

---
+++ precache-manifest.27347d92bcba99a6fa6417fa98842264.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dda374f9043010442aa63ea95e567b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9308862b480679f64e58",
+    "url": "/static/css/main.179f9d59.chunk.css"
+  },
+  {
+    "revision": "a12bd4d90cdd85a3a542",
+    "url": "/static/js/2.3b43cd8f.chunk.js"
+  },
+  {
+    "revision": "9308862b480679f64e58",
+    "url": "/static/js/main.1198558d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
